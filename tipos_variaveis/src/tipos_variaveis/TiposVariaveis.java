b2dfa78dce2fcd755ff4a40d57de7c99ce28c518
Usuario <diego.guidini@hotmail.com>
2024-09-03 16:47:17 -0300

    Teste de primeiro commit

---
+++ tipos_variaveis/src/tipos_variaveis/TiposVariaveis.java
@@ -0,0 +1,12 @@
+package tipos_variaveis;
+
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		
+		System.out.println("Bem vindo a aula de tipos de variáveis");
+			
+		
+	}
+}
+
+
