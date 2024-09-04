19351c63c2b6cdb194b169792384376547032f50
Usuario <diego.guidini@hotmail.com>
2024-09-04 11:08:35 -0300

    commit alteracao

--- tipos_variaveis/src/tipos_variaveis/TiposVariaveis.java
+++ tipos_variaveis/src/tipos_variaveis/TiposVariaveis.java
@@ -3,8 +3,15 @@ package tipos_variaveis;
 public class TiposVariaveis {
 	public static void main(String[] args) {
 		
-		System.out.println("Bem vindo a aula de tipos de variáveis");
-			
+/*TIPOS VARIÁVEIS*/
+		
+		byte idade = 123;
+		short ano = 2024;
+		int cep = 88068201;
+		long cpf = 98395886900L;
+		float pi = 3.14f;
+		double salario = 6500.6;
+		
 		
 	}
 }
