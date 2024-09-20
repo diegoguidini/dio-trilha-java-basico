b0418af79b32bb2193bc80a421da1c317bfd67da
Usuario <diego.guidini@hotmail.com>
2024-09-20 20:32:36 -0300

    commit operadores

---
+++ operadores/src/operadores/operadores.java
@@ -0,0 +1,21 @@
+package operadores;
+
+public class operadores {
+	public static void main(String[] args) throws Exception {
+				
+/*TIPOS VARIÁVEIS*/
+		
+		String nome = "Diego";
+		int idade = 32;
+		char sexo = 'M';
+		boolean doadorOrgao = false;
+		double peso = 70.5;
+		//Date dataNascimento = new Date();
+		
+		System.out.printf("O %S",nome);
+		
+	}
+}
+
+
+
