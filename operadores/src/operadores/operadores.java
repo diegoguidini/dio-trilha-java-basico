1b28bd6017c7d439cee0e2e203ad7a225b7fbe7a
Usuario <diego.guidini@hotmail.com>
2024-09-21 11:17:37 -0300

    Atualizando pacote

--- operadores/src/operadores/operadores.java
+++ operadores/src/operadores/operadores.java
@@ -1,4 +1,5 @@
 package operadores;
+import java.util.Date;
 
 public class operadores {
 	public static void main(String[] args) throws Exception {
@@ -10,9 +11,12 @@ public class operadores {
 		char sexo = 'M';
 		boolean doadorOrgao = false;
 		double peso = 70.5;
-		//Date dataNascimento = new Date();
+		String dataNascimento = "16/01/1992";
+		Date dtregistro = new Date();
 		
-		System.out.printf("O %S",nome);
+		System.out.printf("O %S tem %d anos, pesa %s quilos e é do sexo %s e nasceu em %S. %nÉ doador de orgaos? %s %n"
+				+ "Data execução %S"
+				,nome, idade, peso,sexo,dataNascimento, doadorOrgao,dtregistro);
 		
 	}
 }
