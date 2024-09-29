e4017f9f721ea22c4aa68109a68b27dc283598b1
Usuario <diego.guidini@hotmail.com>
2024-09-29 14:06:53 -0300

    commit desafio

---
+++ conta-banco/src/contaBanco/contaTerminal.java
@@ -0,0 +1,23 @@
+package contaBanco;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class contaTerminal {
+public static void ain(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+    
+	System.out.println("Entre com o número da conta");
+	int contaCorrente = sc.nextInt();
+	
+	System.out.println("Entre com o número da conta");
+	int agencia = sc.nextInt();
+	
+	System.out.println("Entre com o número da conta");
+	int nomeCiente = sc.nextInt();
+	
+	System.out.println("Entre com o número da conta");
+	float saldo = sc.nextInt();
+	
+	}
+}
