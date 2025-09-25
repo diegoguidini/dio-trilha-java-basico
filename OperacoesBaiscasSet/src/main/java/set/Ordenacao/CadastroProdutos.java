1a3fb3b4cdb7ef4dfb1576526e80969807397c17
Diego Guidini <diego.guidini@hotmail.com>
2025-09-25 17:11:05 -0300

    ordenação

---
+++ OperacoesBaiscasSet/src/main/java/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,52 @@
+package main.java.set.Ordenacao;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+	
+	private Set<Produto> produtoSet;
+	
+	public void cadastroProdutos() {
+		// TODO Auto-generated constructor stub
+	this.produtoSet = new HashSet<>();
+	}
+	
+	public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+		produtoSet.add(new Produto(nome, cod, preco, quantidade));
+	}
+	
+	  public Set<Produto> exibirProdutosPorNome() {
+		    Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+		    if (!produtoSet.isEmpty()) {
+		      return produtosPorNome;
+		    } else {
+		      throw new RuntimeException("O conjunto está vazio!");
+		    }
+		  }
+	
+	  public Set<Produto> exibirProdutosPorPreco() {
+		    Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+		    if (!produtoSet.isEmpty()) {
+		      produtosPorPreco.addAll(produtoSet);
+		      return produtosPorPreco;
+		    } else {
+		      throw new RuntimeException("O conjunto está vazio!");
+		    }
+		  }
+	
+	
+	public static void main(String[] args) {
+	    // Criando uma instância da classe CadastroProdutos
+		CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+	    // Adicionando e exibindo produto
+	    cadastroProdutos.adicionarProduto(1, "caneta", 1.50, 1);
+	    cadastroProdutos.adicionarProduto(2, "lápis", 1.00, 2);
+	    cadastroProdutos.adicionarProduto(3, "borracha", 0.50, 3);
+	    cadastroProdutos.adicionarProduto(3, "borracha2", 0.30, 1);
+	    
+	    System.out.println(cadastroProdutos.produtoSet);
+	    
+	}
+}
